4449c54d6ccd49a9aff3fafeea1b3530b22184a4
Sebastian Vatland Sonesen <sebastian.sonesen@bouvet.no>
2020-11-23 13:22:09 +0100

    Creates index and error view

---
+++ WishList/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WishList.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return View();
+            return View("~/Views/Home/Index.cshtml");
+ 
+        }
+
+
+        public IActionResult Error()
+        {
+            //return View();
+            return View("~/Views/Shared/Error.cshtml");
+        }
+    }
+}
